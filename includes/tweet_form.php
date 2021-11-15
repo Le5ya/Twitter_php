edc6fff7fa3057606b5440b9f544b5c4a4dbbfd1
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-11-15 22:13:44 +0300

    add and delete likes

--- includes/tweet_form.php
+++ includes/tweet_form.php
@@ -1,13 +1,13 @@
 		<section class="wrapper">
-			<form class="tweet-form" action="<?php acho get_url('includes/add_post.php'); ?>"
+			<form class="tweet-form" action="<?php echo get_url('includes/add_post.php'); ?>"
 				method="post">
 				<div class="tweet-form__wrapper">
-					<img class="avatar" src="<?php echo get_url($_SERVER['user']['avatar']); ?>" alt="Аватар">
+					<img class="avatar" src="<?php echo get_url($_SESSION['user']['avatar']); ?>" alt="Аватар">
 					<textarea class="tweet-form__text" rows="4" placeholder="Что происходит?" name="text"required></textarea>
 				</div>
 				<div class="tweet-form__btns">
 					<button class="tweet-img__btn" type="button"></button>
-					<span id="image-span"><image src="https://static.wikia.nocookie.net/dayr/images/b/b8/%D0%93%D0%BE%D1%80%D1%8B.png/revision/latest?cb=20200125122904&path-prefix=ru"></span>
+					<span id="image-span"><image src="images/sun.png"></span>
 					<input type="hidden" id="image-path" name="image">
 					<button class="tweet-form__btn" type="submit">Твитнуть</button>
 				</div>
